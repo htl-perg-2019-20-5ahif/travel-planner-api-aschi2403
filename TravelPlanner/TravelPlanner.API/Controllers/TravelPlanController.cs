e7b81fe40ba5dbf84fb1ea454b052c0340a9e902
aschi2403 <jakob@familie-aschauer.eu>
2019-10-23 07:36:41 +0200

    working programm

---
+++ TravelPlanner/TravelPlanner.API/Controllers/TravelPlanController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TravelPlanner.Logic;
+
+namespace TravelPlanner.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TravelPlanController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly HttpClient client;
+        private readonly IEnumerable<Route> routes;
+        private readonly ConnectionFinder connectionFinder;
+
+        public TravelPlanController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+            client = clientFactory.CreateClient();
+            routes = GetRoutesFromAPIAsync().Result;
+            connectionFinder = new ConnectionFinder(routes);
+        }
+
+        [HttpGet]
+       public ActionResult<TripWithLocationsAPIResult> GetTripWithLocations(string from, string to, string start)
+        {
+            var connectionResult = connectionFinder.FindConnection(from, to, start);
+            if (connectionResult == null)
+                return NotFound();
+            return ConvertConnectionResult(connectionResult);
+        }
+
+        private TripWithLocationsAPIResult ConvertConnectionResult(TripWithLocations connectionResult)
+        {
+            return new TripWithLocationsAPIResult()
+            {
+                Depart = connectionResult.FromCity,
+                DepartureTime = connectionResult.Leave,
+                Arrive = connectionResult.ToCity,
+                ArrivalTime = connectionResult.Arrive
+            };
+        }
+
+        private async Task<IEnumerable<Route>> GetRoutesFromAPIAsync()
+        {
+            var routesResponse = await client.GetAsync("https://cddataexchange.blob.core.windows.net/data-exchange/htl-homework/travelPlan.json");
+            routesResponse.EnsureSuccessStatusCode();
+            var responseBody = await routesResponse.Content.ReadAsStringAsync();
+            var routes = JsonSerializer.Deserialize<Route[]>(responseBody);
+            return routes;
+        }
+    }
+}
---
+++ TravelPlanner/TravelPlanner.API/TripWithLocationsAPIResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace TravelPlanner.API
+{
+    public class TripWithLocationsAPIResult
+    {
+        [JsonPropertyName("depart")]
+        public string Depart { get; set; }
+        [JsonPropertyName("departureTime")]
+        public string DepartureTime { get; set; }
+        [JsonPropertyName("arrive")]
+        public string Arrive { get; set; }
+        [JsonPropertyName("arrivalTime")]
+        public string ArrivalTime { get; set; }
+    }
+}
